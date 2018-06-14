cdcc190de5e27f9b33c197d8ab16256efc96ccd1
user1 <user1@user1s-MacBook-Air.local>
2018-06-14 12:51:36 -0500

    Checkpoint 8 events: completed

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -5,7 +5,7 @@ class ToDo extends Component {
     render() {
         return (
             <li>
-              <input type="checkbox" checked={ this.props.isCompleted }/>
+              <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
               <span>{ this.props.description }</span>
             </li>
         );
